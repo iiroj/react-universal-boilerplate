13178e2a5bb6bdebbe10c516b25e2e4de38713c2
Iiro Jäppinen <iiro@jappinen.fi>
2018-07-11 14:34:06 +0300

    Initialize route server-side

--- src/server/utils/configureStore.js
+++ src/server/utils/configureStore.js
@@ -1,4 +1,4 @@
-import { routerForExpress } from 'redux-little-router';
+import { initializeCurrentLocation, routerForExpress } from 'redux-little-router';
 
 import { routesByPath } from '../../client/routes';
 import configureStore from '../../client/store';
@@ -6,6 +6,14 @@ import configureStore from '../../client/store';
 export default async (request, response) => {
   const router = routerForExpress({ routes: routesByPath, request });
   const store = configureStore(router);
+
+  const state = store.getState();
+
+  store.dispatch(initializeCurrentLocation(state.router));
+
+  if (!state.router.route) {
+    response.status(404);
+  }
 
   return store;
 };
