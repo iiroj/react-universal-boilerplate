7c6fc93800285e1bbba764ca13d2d5372cf1929d
Iiro Jäppinen <iiro@jappinen.fi>
2018-09-15 11:07:12 +0300

    refactor: Simplify index

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -12,8 +12,7 @@ import App from './components/App';
 
 const history = createHistory();
 const state = JSON.parse(document.getElementById('initial-state').innerHTML);
-const store =
-  module.hot && module.hot.data && module.hot.data.store ? module.hot.data.store : configureStore(history, state).store;
+const store = configureStore(history, state).store;
 
 const render = process.env.NODE_ENV === 'production' ? ReactDOM.hydrate : ReactDOM.render;
 
@@ -28,8 +27,4 @@ render(
 
 if (module.hot) {
   module.hot.accept();
-
-  module.hot.dispose(data => {
-    data.store = store;
-  });
 }
