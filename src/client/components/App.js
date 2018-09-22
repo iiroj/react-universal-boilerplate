184f6433186c57b5b8da4df43431a64e008b3f41
Iiro Jäppinen <iiro@jappinen.fi>
2018-09-22 16:41:43 +0300

    refactor: UniversalComponent receives import as prop

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,97 +1,48 @@
-import FontFaceObserver from 'fontfaceobserver';
-import React from 'react';
-import { injectGlobal } from 'emotion';
-import reset from 'css-wipe/js';
 import PropTypes from 'prop-types';
+import React from 'react';
 import { connect } from 'react-redux';
 
-import PageLoader from '../pages';
+import Layout from './Layout';
+import UniversalComponent from './UniversalComponent';
 
-const plex300 = new FontFaceObserver('IBM Plex Sans', { weight: 300 });
-const plex600 = new FontFaceObserver('IBM Plex Sans', { weight: 600 });
+class App extends React.Component {
+  static propTypes = {
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired
+    }).isRequired,
+    page: PropTypes.shape({
+      component: PropTypes.string.isRequired
+    }).isRequired
+  };
 
-class App extends React.PureComponent {
-  componentDidMount() {
-    Promise.all([plex300.load(), plex600.load()]);
+  state = {
+    loading: false
+  };
+
+  setLoading = () => this.setState({ loading: true });
+
+  setNotLoading = () => this.setState({ loading: false });
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
   }
 
   render() {
     const { page } = this.props;
 
-    return <PageLoader page={page} />;
+    return (
+      <Layout>
+        <UniversalComponent
+          onBefore={this.setLoading}
+          onAfter={this.setNotLoading}
+          page={page}
+          src={() => import(`../pages/${page.component}`)}
+        />
+      </Layout>
+    );
   }
 }
 
-App.propTypes = {
-  page: PropTypes.shape({
-    path: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    component: PropTypes.string.isRequired
-  }).isRequired
-};
-
-injectGlobal(
-  reset,
-  {
-    '@font-face': {
-      fontFamily: 'IBM Plex Sans',
-      fontStyle: 'normal',
-      fontWeight: 300,
-      src:
-        'local("IBM Plex Sans Light"), local("IBMPlexSans-Light"), url(https://fonts.gstatic.com/s/ibmplexsans/v3/zYX9KVElMYYaJe8bpLHnCwDKjXr8AIFsdP3pBms.woff2) format("woff2")',
-      unicodeRange:
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD'
-    }
-  },
-  {
-    '@font-face': {
-      fontFamily: 'IBM Plex Sans',
-      fontStyle: 'normal',
-      fontWeight: 600,
-      src:
-        'local("IBM Plex Sans SemiBold"), local("IBMPlexSans-SemiBold"), url(https://fonts.gstatic.com/s/ibmplexsans/v3/zYX9KVElMYYaJe8bpLHnCwDKjQ76AIFsdP3pBms.woff2) format("woff2")',
-      unicodeRange:
-        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD'
-    }
-  },
-  {
-    html: {
-      height: '100%'
-    },
-
-    '#root': {
-      alignItems: 'center',
-      backgroundImage: 'linear-gradient(-153deg, rgb(45, 45, 45) 0%, rgb(0, 0, 0) 95%)',
-      color: 'white',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      minHeight: '100%',
-      padding: '2rem',
-      width: '100%'
-    },
-
-    body: {
-      fontFamily: '"IBM Plex Sans", sans-serif',
-      fontSize: 20,
-      fontWeight: 300,
-      height: '100%',
-      overflowX: 'hidden',
-      overflowY: 'auto'
-    },
-
-    '*': {
-      lineHeight: '2rem'
-    },
-
-    strong: {
-      fontWeight: 600
-    },
-
-    em: {
-      fontStyle: 'italic'
-    }
-  }
-);
-
-export default connect(({ page }) => ({ page }))(App);
+export default connect(state => state)(App);
---
+++ src/client/components/Layout.js
@@ -0,0 +1,86 @@
+import FontFaceObserver from 'fontfaceobserver';
+import React from 'react';
+import { injectGlobal } from 'emotion';
+import reset from 'css-wipe/js';
+import PropTypes from 'prop-types';
+
+const plex300 = new FontFaceObserver('IBM Plex Sans', { weight: 300 });
+const plex600 = new FontFaceObserver('IBM Plex Sans', { weight: 600 });
+
+export default class Layout extends React.PureComponent {
+  static propTypes = {
+    children: PropTypes.any.isRequired
+  };
+
+  componentDidMount() {
+    Promise.all([plex300.load(), plex600.load()]);
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+injectGlobal(
+  reset,
+  {
+    '@font-face': {
+      fontFamily: 'IBM Plex Sans',
+      fontStyle: 'normal',
+      fontWeight: 300,
+      src:
+        'local("IBM Plex Sans Light"), local("IBMPlexSans-Light"), url(https://fonts.gstatic.com/s/ibmplexsans/v3/zYX9KVElMYYaJe8bpLHnCwDKjXr8AIFsdP3pBms.woff2) format("woff2")',
+      unicodeRange:
+        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD'
+    }
+  },
+  {
+    '@font-face': {
+      fontFamily: 'IBM Plex Sans',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      src:
+        'local("IBM Plex Sans SemiBold"), local("IBMPlexSans-SemiBold"), url(https://fonts.gstatic.com/s/ibmplexsans/v3/zYX9KVElMYYaJe8bpLHnCwDKjQ76AIFsdP3pBms.woff2) format("woff2")',
+      unicodeRange:
+        'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD'
+    }
+  },
+  {
+    html: {
+      height: '100%'
+    },
+
+    '#root': {
+      alignItems: 'center',
+      backgroundImage: 'linear-gradient(-153deg, rgb(45, 45, 45) 0%, rgb(0, 0, 0) 95%)',
+      color: 'white',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      minHeight: '100%',
+      padding: '2rem',
+      width: '100%'
+    },
+
+    body: {
+      fontFamily: '"IBM Plex Sans", sans-serif',
+      fontSize: 20,
+      fontWeight: 300,
+      height: '100%',
+      overflowX: 'hidden',
+      overflowY: 'auto'
+    },
+
+    '*': {
+      lineHeight: '2rem'
+    },
+
+    strong: {
+      fontWeight: 600
+    },
+
+    em: {
+      fontStyle: 'italic'
+    }
+  }
+);
